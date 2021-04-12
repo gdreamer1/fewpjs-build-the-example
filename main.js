19539aab8425a1b73a62fb927079d816ffbf0567
gdreamer1 <gdadreamer@gmail.com>
2021-04-11 22:10:27 -0700

    assinment

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
+const colorStates = {
+  "red" : "",
+  "": "red"
+};
+const articleHearts = document.querySelectorAll(".like-glyph");
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall("bogusUrl")
+    .then(function(serverMessage){
+       heart.innerText = glyphStates[heart.innerText];
+       heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = ".hidden", 3000);
+    });
+}
 
-// Your JavaScript code goes here!
-
-
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 
 //------------------------------------------------------------------------------
